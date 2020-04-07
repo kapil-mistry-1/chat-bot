0ed346b37dd84b5bd005e69a6dd039fa8be91eaa
swapnilalmeida <swapnil.almeida@lntinfotech.com>
2020-04-07 12:21:10 +0530

    Ask Me and chat window

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  openChatBox: boolean = false;
+  bgoverly: boolean = true;
+
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
+    // this.renderer.addClass(document.body, "transparentbg");
+  }
+  
+  showChatBox(){
+    this.openChatBox = true;
+    this.bgoverly = false;
+  }
+  closeChatBox() {
+    this.openChatBox = false;
+    // this.renderer.removeClass(document.body, "transparentbg");
   }
 
 }
